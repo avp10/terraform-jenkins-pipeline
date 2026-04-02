859729314f734b6e7d03af42f6165e00eb4d95ee
avp10 <77101200+avp10@users.noreply.github.com>
2026-04-02 22:13:29 +0530

    change

--- variables.tf
+++ variables.tf
@@ -13,14 +13,14 @@ variable "aws_secret_key" {
 variable "aws_region" {
   description = "AWS region"
   type        = string
-  default     = "ap-south-1"
+  default     = "us-east-1"
 }
 
 
 variable "ami" {
    type        = string
    description = "Ubuntu AMI ID"
-   default     = "ami-0931307dcdc2a28c9"
+   default     = "ami-0ec10929233384c7f"
 }
 
 variable "instance_type" {
